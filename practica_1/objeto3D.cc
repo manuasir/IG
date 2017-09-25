34476ed66c00c855cf57eefb652185bc46962167
Manuel <manu.asi2@gmail.com>
2017-09-25 17:19:21 +0200

    new classes

---
+++ practica_1/objeto3D.cc
@@ -0,0 +1,10 @@
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include "Objeto3D.h"
+
+Objeto3D::Objeto3D(){
+}
+
---
+++ practica_1/objeto3D.h
@@ -0,0 +1,12 @@
+#ifndef _OBJETO3D_H
+#define _OBJETO3D_H
+
+class Objeto3D {
+private:
+
+public:
+	Objeto3D();
+	void construye(init);
+	void visualiza(draw);
+};
+#endif
